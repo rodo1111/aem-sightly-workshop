2775273e55918a3960cee6c96496a9399ac0cd6f
Rodolfo Oviedo <rodolfo.oviedo@prodigious.cr>
2015-02-11 19:47:35 -0600

    Initial version of the AEM project

---
+++ bundle/src/main/java/cr/prodigious/aem/sightlyworkshop/HelloService.java
@@ -0,0 +1,13 @@
+package cr.prodigious.aem.sightlyworkshop;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
---
+++ bundle/src/main/java/cr/prodigious/aem/sightlyworkshop/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package cr.prodigious.aem.sightlyworkshop;
+
+import aQute.bnd.annotation.Version;
